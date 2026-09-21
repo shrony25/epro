356bd476b8cc11f36a16a250998ed65156078111
shrony25 <srony25@gmail.com>
2026-09-21 13:43:19 +0600

    Add files via upload

---
+++ download/evaluation-pro.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Evaluation Pro
+// @namespace    shahin.lged.eprocure.pro
+// @version      1.11
+// @description  Auto Evaluation + Clarification + JV Control + Finalize Responsiveness - Licensed Lifetime Edition by Mohammad Shahin Hossain, LGED
+// @author       Mohammad Shahin Hossain
+// @copyright    2025 Mohammad Shahin Hossain, Surveyor, LGED. All Rights Reserved.
+// @license      Commercial - Lifetime Single Device - srony25@gmail.com
+// @match        https://www.eprocure.gov.bd/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @connect      shrony25.github.io
+// @run-at       document-idle
+// @updateURL    https://shrony25.github.io/epro/evaluation-pro.meta.js
+// @downloadURL  https://shrony25.github.io/epro/evaluation-pro.user.js
+// @supportURL   mailto:srony25@gmail.com
+// ==/UserScript==
